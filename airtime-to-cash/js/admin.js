ff57585a918901acb826cb813dc97eab0035b7d6
odedinaemmanuel84-dev <odedinaemmanuel84@gmail.com>
2026-08-02 23:53:14 +0100

    Update admin.js

--- airtime-to-cash/js/admin.js
+++ airtime-to-cash/js/admin.js
@@ -1,4 +1,16 @@
 // ==========================================
+// ADMIN AUTH CHECK
+// ==========================================
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+if (!user || user.role !== "admin") {
+
+    window.location.href = "login.html";
+
+}
+
+// ==========================================
 // CONFIG
 // ==========================================
 
